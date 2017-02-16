5a808fda93844fe984ed5a70296c16a74256d5c4
Collin Brooks <cbrooks816@yahoo.com>
2017-02-16 04:49:42 -0600

    blogs can be submitted and are sorted in descending order

--- build-a-blog/main.py
+++ build-a-blog/main.py
@@ -1,24 +1,48 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import jinja2
+import os
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Blog(db.Model):
+    topic = db.StringProperty(required = True)
+    blog = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+
+class MainHandler(Handler):
+    def render_post_form(self, topic = "", blog = "", error = ""):
+        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 5;")
+        self.render("new_post_form.html", topic = topic, blog = blog, error = error, blogs = blogs)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_post_form()
+
+    def post(self):
+        topic = self.request.get("topic")
+        blog = self.request.get("blog")
+
+        if topic and blog:
+            blog = Blog(topic = topic, blog = blog)
+            blog.put()
+
+            self.redirect("/")
+        else:
+            error = "please enter both a topic and a blog"
+            self.render_post_form(topic, blog, error)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
